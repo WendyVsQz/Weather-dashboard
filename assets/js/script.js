bde68534c1734692d167114282e6b459140252af
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-02-20 16:27:55 +0000

    storing data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-// API Key: a43fc3b983181afb01b8329075754564
-// Current weather call: 
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a43fc3b983181afb01b8329075754564
-
-// ----------
-
 /* // Div tags to consider: 
 * 1- Search for city: '#search-city', '#search-button', '#button', '#clear-history', '#list-group'
 * 2- Today's weather info: '#today', '#region', '#polite', '#current'
@@ -13,9 +7,24 @@
 
 // 1. Local storage
 
+document.getElementById('search_button').addEventListener("click", displayCity);
+
+function displayCity(){
+  var search_city = document.querySelector('#search_city').value;
+  localStorage.setItem('Is_search_city', search_city );
+  localStorage.getItem('Is_search_city');
+  
+}
+
+document.getElementById('clear_history').addEventListener('click', clearHistory);
+
+function clearHistory(){
+  var clear_history = document.querySelector('#clear_history');
+  localStorage.clear(clear_history);
+}
 
 
-//moment.js
+// 2. moment.js
 
 var timeDisplayEl = document.querySelectorAll(['#current','#tomorrow','#next', '#third', '#forth', '#fifth']);
 for (var i = 0; i < timeDisplayEl.length; i++){
@@ -31,16 +40,7 @@ for (var i = 0; i < timeDisplayEl.length; i++){
 
 // 3. API
 
-// Here we display the 5 days forecast for the current city.
-//Set up the API key
-
-
-/* regionData()
-function regionData() {
-  navigator.geolocation.regionData((success) => {
-    console.log(success);
-  })
-} */
+// 3.1. get the geolocation
 
 const API_key = "97f51d5e48b2c8cbef33200ea2f10ece";
 function getCityCoor(){
@@ -48,7 +48,6 @@ var city = "London"
 $.ajax({
     method: 'GET',
     url: 'http:api.openweathermap.org/geo/1.0/direct?q='+city+ '&appid='+ API_key,
-    //contentType: 'application/json',
     success: function(result) {
         getWeather(result[0].lat,result[0].lon )
     },
@@ -56,16 +55,13 @@ $.ajax({
         console.error('Error: ', jqXHR.responseText);
     }
 });
-//fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+ '&limit=5&appid='+ API_key,)
-//.then(response=> {
-  //console.log(response)
-//})
 }
 
+// 3.2. display current weather
 function getWeather(lat, lon){
   $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=97f51d5e48b2c8cbef33200ea2f10ece',
+    url: 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+ API_key,
     success: function(result) {
       displayWeather(result)
   },
@@ -75,12 +71,6 @@ function getWeather(lat, lon){
 
   })
 }
-
-/* obj("lisa= 9", "bart=10", "Homer=50")
-"obj.lisa"
-
-Arr["lisa", "bart", "Homer"]
-Arr[0]; */
 
 function displayWeather(data){
   console.log(data)
@@ -88,29 +78,24 @@ function displayWeather(data){
   console.log(data.main.temp)
   console.log(data.wind)
   console.log(data.main.humidity)
-  console.log(data.sys)
-  document.querySelector(".temp").textContent = "Temp: "+data.main.temp
-  document.querySelector(".wind").textContent="Wind: "+data.wind.deg
-  document.querySelector(".humidity").textContent="Humidity: "+ data.main.humidity
-
-
-  //id="awesome"
-  //document.querySelector("#awesome")
-  //document.getElementById("awesome")
+  console.log(data.sys)  
+  document.querySelector("#temp").textContent = "Temp: "+data.main.temp
+  document.querySelector("#wind").textContent="Wind: "+data.wind.deg
+  document.querySelector("#humidity").textContent="Humidity: "+ data.main.humidity
 }
 
+// 3.3.  display 5 days forecast
+/* 
 function getWeather(lat, lon){
   $.ajax({
     method: 'GET',
-    url: 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat='+lat+'&lon='+lon+'&appid=97f51d5e48b2c8cbef33200ea2f10ece',
+    url: 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+ API_key,
     success: function(result) {
       displayWeather(result)
   },
   error: function(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
   }
-
-  //'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat='+lat+'&lon='+lon+'&appid=97f51d5e48b2c8cbef33200ea2f10ece
   })
 }
 
@@ -118,7 +103,4 @@ function displayForecast(data){
 
 }
 
-getCityCoor()
-
-localStorage.setItem('TEST', "Hello")
-localStorage.getItem("TEST")
+getCityCoor() */

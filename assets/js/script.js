57f9a01ef3837aedbdb35335eb92b6883033e991
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-05-11 17:59:52 +0100

    weather api done

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//TAGS: #Today, #Tomorrow, yesterday,
+//#card, #search, #search-bar, #forecast,
+//#weather, #city, #temp, #icon, #description, #humidity, #wind.
+//---------------------------------------------------------------//
+//API Key: 747b155a3ad134502ce1104de711994f
+
+let weather= {
+    // 01. Getting API
+    apiKey: "747b155a3ad134502ce1104de711994f",
+    //02. Fetch city
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city + "&units=metric&appid=" + this.apiKey
+        ).then((response)=> response.json())
+        .then((data)=> this.displayWeather(data));
+    },
+    //03. function data
+    displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    //04. Class target
+    document.querySelector(".city").innerText= "Weather in " + name;
+    document.querySelector(".icon").src = 
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "wind speed: " + speed + "km/h";
+    }
+
+    
+};
